5b307d10c6c9d6969d8e3296408271f9d20b6d5f
demoffsrmain <ustimdima2002@gmail.com>
2026-04-05 14:15:00 +0200

    Initial Commit

---
+++ Vox/VoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoxApp.swift
+//  Vox
+//
+//  Created by Dmitry Demidov on 05.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct VoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
